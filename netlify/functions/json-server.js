8ab75de7c21abe4a4894f4ed6703ca02d34c34b2
kayanja2024 <paulkayanja1989@gmail.com>
2025-03-04 18:47:57 +0300

    Added JSON Server to Netlify functions

--- netlify/functions/json-server.js
+++ netlify/functions/json-server.js
@@ -1,14 +1,9 @@
-const jsonServer = require('json-server');
-const serverless = require('serverless-http');
-
-// Create a JSON server instance
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // Path to your JSON file
+const router = jsonServer.router("db.json"); // Your JSON data file
 const middlewares = jsonServer.defaults();
 
-// Set up middlewares
 server.use(middlewares);
 server.use(router);
 
-// Export the handler for Netlify's serverless function
-module.exports.handler = serverless(server);
+module.exports = server;
